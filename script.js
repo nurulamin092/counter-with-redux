4cdb00d00f774fbf8b3d43ce5c2538e11cc10fd7
nurulamin092 <mnurulamin092@gmial.com>
2022-08-14 09:51:03 +0600

    cunter app with redux

---
+++ script.js
@@ -0,0 +1,61 @@
+
+// select dom elements
+
+const counterEl = document.getElementById("counter");
+const incrementEl = document.getElementById("increment");
+const decrementEl = document.getElementById("decrement");
+
+
+// initial State
+
+const initialState = {
+    value: 0,
+};
+
+// create reduce function
+
+function counterReducer(state = initialState, action) {
+    if (action.type === 'increment') {
+        return {
+            ...state,
+            value: state.value + 1,
+        }
+    } else if (action.type == 'decrement') {
+        return {
+            ...state,
+            value: state.value - 1,
+        }
+    }
+    else {
+        return state;
+    }
+}
+
+// create redux store 
+
+const store = Redux.createStore(counterReducer);
+
+const render = () => {
+    const state = store.getState();
+    counterEl.innerText = state.value.toString();
+}
+render()
+
+store.subscribe(render);
+
+// button click listeners
+
+incrementEl.addEventListener("click", () => {
+    store.dispatch({
+        type: 'increment',
+    });
+});
+
+decrementEl.addEventListener("click", () => {
+    store.dispatch({
+        type: 'decrement',
+    });
+});
+
+
+
